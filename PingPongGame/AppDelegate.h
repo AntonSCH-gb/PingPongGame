e469c084695fd51b381cc0d1a0b29df342e825ad
AntonSCH <AntonSCH@mail.ru>
2021-04-26 01:48:14 +0300

    Initial Commit

---
+++ PingPongGame/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PingPongGame
+//
+//  Created by Anton Scherbaev on 26.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PingPongGame/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  PingPongGame
+//
+//  Created by Anton Scherbaev on 26.04.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
